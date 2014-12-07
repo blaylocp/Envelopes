ae93d748a1556bdea0cc6ff668b13d95838c3aee
Preston Blaylock <Preston@Prestons.local>
2014-12-06 19:55:39 -0700

    sql

---
+++ sql/envelopes.sql
@@ -0,0 +1,13 @@
+DROP DATABASE Envelopes;
+
+CREATE DATABASE Envelopes;
+
+USE envelopes;
+
+
+CREATE TABLE envelope
+( category_id INT PRIMARY KEY AUTO_INCREMENT
+, category_name VARCHAR(20) NOT NULL
+, envelope_amount Decimal(10,2)
+, customer_username VARCHAR(20)
+);
